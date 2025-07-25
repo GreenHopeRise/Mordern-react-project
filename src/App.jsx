bc2999b8e181308f4af1d0f38e62a2e41d1992aa
GreenHopeRise <khalidanwarantur96@gmail.com>
2025-07-26 01:42:56 +0600

    hero section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-
-import Navbar from './components/Navbar'
-import './index.css'
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import "./index.css";
 
 function App() {
-
-
   return (
-    <>
-      <Navbar/>
-    </>
-  )
+    <main className="relative min-h-screen overflow-x-hidden">
+      <div className="absolute -top-28 -left-28 w-[500px] h-[500px] bg-gradient-to-tr from-indigo-500/20 to-pink-500/20 rounded-full blur-[80px] -z-10"></div>
+      <div className="overflow-hidden">
+        <Navbar />
+        <Hero />
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
