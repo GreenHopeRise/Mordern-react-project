4880234e1bf5f7c16db0c88e581d84c951a12467
GreenHopeRise <khalidanwarantur96@gmail.com>
2025-07-29 23:07:40 +0600

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { HiMenu, HiX } from "react-icons/hi";
+import { motion } from "motion/react"
+
 
 const Navbar = () => {
   const [activeLink, setActiveLink] = useState("#home");
@@ -25,7 +27,9 @@ const Navbar = () => {
     },
   ];
   return (
-    <nav className="fixed top-0 left-0 right-0 bg-white/90 backdrop-blur-sm z-50 border-b border-gray-100 shadow-sm ">
+    <motion.nav
+    
+    className="fixed top-0 left-0 right-0 bg-white/90 backdrop-blur-sm z-50 border-b border-gray-100 shadow-sm ">
       <div className="w-full container mx-auto flex items-center justify-between px-4 sm:px-6 lg:px-8 md:h-20 h-16">
         {/* logo */}
         <div className=" flex items-center gap-1 cursor-pointer">
@@ -96,7 +100,7 @@ const Navbar = () => {
           </div>
         </div>
       )}
-    </nav>
+    </motion.nav>
   );
 };
 
